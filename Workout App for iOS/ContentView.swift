f1f76931a1134200fc0bfc77030a905bb60b3935
jacobcwildes <wildes126@gmail.com>
2025-01-10 09:55:15 -0500

    Initial Commit

---
+++ Workout App for iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Workout App for iOS
+//
+//  Created by Jacob Wildes on 1/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Workout App for iOS/Workout_App_for_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_App_for_iOSApp.swift
+//  Workout App for iOS
+//
+//  Created by Jacob Wildes on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Workout_App_for_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
